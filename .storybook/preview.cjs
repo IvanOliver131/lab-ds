508d8dccbd3d02a3d26c40cc08eab3a9311c1ac4
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-14 14:24:06 -0300

    finish v2.0

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -2,6 +2,14 @@ import { themes } from "@storybook/theming";
 
 import "../src/styles/global.css";
 
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize({
+  onUnhandledRequest: 'bypass'
+});
+
+export const decorators = [mswDecorator];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- src/pages/SignIn/SignIn.stories.tsx
+++ src/pages/SignIn/SignIn.stories.tsx
@@ -1,4 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { rest } from "msw";
 import { SignIn } from ".";
 
 export default {
@@ -6,6 +9,36 @@ export default {
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  // Dentro desta função realizamos os testes
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "ivanoliver131@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("************"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() => {
+      expect(canvas.getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
